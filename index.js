af99d88e4ffcc8408f14a6cfcf4d468fe03a7a42
Muhammed Yayla <muhammeddyayla@gmail.com>
2023-03-05 01:47:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function scrollIntoServices() {
+    document.getElementById("services").scrollIntoView({
+      behavior: 'auto',
+      block: 'center',
+      inline: 'center',
+      behavior: 'smooth'
+    });
+}
+
+function scrollIntoAbout() {
+  document.getElementById("about").scrollIntoView({
+    behavior: 'auto',
+    block: 'center',
+    inline: 'center',
+    behavior: 'smooth'
+  });
+}
